4fde49fb84d76e0b1017cc15c3f0373b93fb21ac
Liliana <lilkupczyk@gmail.com>
2024-08-09 15:07:22 +0200

    updated index.js

--- back-end/index.js
+++ back-end/index.js
@@ -5,17 +5,13 @@ const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
 const port = 5000
-
 server.use(cors())
 server.use(middlewares)
-
 server.use((req, res, next) => {
 	req.db = router.db
 	next()
 })
-
 server.use(router)
-
 server.listen(port, () => {
 	console.log(`JSON Server is running on PORT ${port}`)
 })
