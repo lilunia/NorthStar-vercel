356d33829c7767093b4fd9e12f0362d264ac09c7
Liliana <lilkupczyk@gmail.com>
2024-08-10 18:42:15 +0200

    modified backend config files

--- back-end/api/index.js
+++ back-end/api/index.js
@@ -1,8 +1,10 @@
 const jsonServer = require('json-server')
 const cors = require('cors')
 const server = jsonServer.create()
-const router = jsonServer.router('db.json')
+const path = require('path');
+// const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
+const router = jsonServer.router(path.join(__dirname, 'db.json'));
 
 const port = 5000
 server.use(cors())
@@ -15,3 +17,5 @@ server.use(router)
 server.listen(port, () => {
 	console.log(`JSON Server is running on PORT ${port}`)
 })
+
+module.exports = server;
